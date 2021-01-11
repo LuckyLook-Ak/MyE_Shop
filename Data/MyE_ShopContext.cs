8e0598fbbfb251c12833cb74e3dce260ce114e9c
Apostolos Koukoulios <apostoak@gmail.com>
2021-01-11 22:11:33 +0200

    Added Models and Controllers for --> Products and Customers also updated the layout page.

---
+++ Data/MyE_ShopContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MyE_Shop.Data
+{
+    public class MyE_ShopContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public MyE_ShopContext() : base("name=MyE_ShopContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<MyE_Shop.Models.Product> Products { get; set; }
+
+        public System.Data.Entity.DbSet<MyE_Shop.Models.Order> Orders { get; set; }
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyE_Shop.Models
+{
+    public class Order
+    {
+        public int ID { get; set; }
+        public int ProductID { get; set; }
+        public Product product { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
